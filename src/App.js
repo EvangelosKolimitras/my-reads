e6b3846b062d5c7f659b724e701a50ab405ae358
Evangelos Kolimitras <evangeloskolimitras@outlook.com>
2020-12-05 09:37:32 +0100

    fix: books are now moved to other shelfs and not copied

--- src/App.js
+++ src/App.js
@@ -1,66 +1,83 @@
-import React from 'react';
-import './App.css';
-import { books } from './components/books'
-import SearchBar from './components/SearchBar';
-import Bookcase from './components/Bookcase'
+import React from "react";
+import "./App.css";
+import { books } from "./components/books";
+import SearchBar from "./components/SearchBar";
+import Bookcase from "./components/Bookcase";
 import { Link, Route } from "react-router-dom";
 class BooksApp extends React.Component {
-
   state = {
     bookcase: {
+      // Edo ine ta state gia ta 3 rafia
+      books,
       currentlyReading: [],
       wantToRead: [],
-      read: [],
+      read: []
     },
     selectValue: ""
-  }
+  };
 
   componentDidMount() {
     // State initialisation
-    this.setState(prevState => {
+    this.setState((prevState) => {
       return {
+        books,
         bookcase: {
-          currentlyReading: books.filter(book => book.shelf === "currentlyReading"),
-          wantToRead: books.filter(book => book.shelf === "wantToRead"),
-          read: books.filter(book => book.shelf === "read"),
+          currentlyReading: books.filter(
+            (book) => book.shelf === "currentlyReading"
+          ),
+          wantToRead: books.filter((book) => book.shelf === "wantToRead"),
+          read: books.filter((book) => book.shelf === "read")
         }
-      }
-    })
+      };
+    });
   }
 
   changeShelfHandler = (e, book) => {
-    const value = e.target.value
+    const value = e.target.value;
 
-    const books = [...this.state.bookcase[book.shelf]]
+    const books = [...this.state.bookcase[book.shelf]];
     console.log(books);
-    const filteredBooks = books.filter(b => b.id !== book.id)
-    console.log(filteredBooks);
+    const filteredBooks = books.filter((b) => b.id !== book.id);
+    console.log("shelf to change", value);
 
-
-    this.setState(prevState => {
-      if (value === "none") return
+    this.setState((prevState) => {
+      if (value === "none") return;
 
       return {
+        books: prevState.books.map((b) =>
+          b.id === book.id ? { ...b, shelf: value } : b
+        ),
         bookcase: {
+          // ine sosti i logiki edo?
           ...prevState.bookcase,
           [book.shelf]: filteredBooks,
-          [value]: [...prevState.bookcase[value], book]
+          [value]: [...prevState.bookcase[value], { ...book, shelf: value }]
         },
         selectValue: ""
-      }
-    })
-  }
+      };
+    });
+  };
 
   render() {
     const {
       bookcase: { currentlyReading, wantToRead, read }
-    } = this.state
+    } = this.state;
 
     const categories = [
-      { id: Math.random() * 19, category: "Reading", shelf: "currentlyReading", books: currentlyReading },
-      { id: Math.random() * 19, category: "To read", shelf: "wantToRead", books: wantToRead },
+      {
+        id: Math.random() * 19,
+        category: "Reading",
+        shelf: "currentlyReading",
+        books: currentlyReading
+      },
+      {
+        id: Math.random() * 19,
+        category: "To read",
+        shelf: "wantToRead",
+        books: wantToRead
+      },
       { id: Math.random() * 19, category: "Read", shelf: "read", books: read }
-    ]
+    ];
 
     return (
       <div className="app">
@@ -69,22 +86,21 @@ class BooksApp extends React.Component {
         <Route
           exact
           path="/"
-          render={() =>
+          render={() => (
             <Bookcase
               categories={categories}
               changeShelfHandler={this.changeShelfHandler}
               selectValue={this.state.selectValue}
             />
-          }
+          )}
         />
 
         <div className="open-search">
           <Link to="/search">Add a book </Link>
         </div>
       </div>
-    )
+    );
   }
 }
 
-
-export default BooksApp
+export default BooksApp;
--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -1,34 +1,33 @@
-import React from 'react'
-import { Book } from './Book';
+import React from "react";
+import { Book } from "./Book";
 
-const Shelf = props => {
+const Shelf = (props) => {
 	return (
 		<div>
 			<div className="bookshelf">
 				<h2 className="bookshelf-title">{props.shelf.category}</h2>
 				<div className="bookshelf-books">
-
 					{/* Render the list of books per shelf */}
 					<ol className="books-grid">
-						{props.shelf?.books.map(book => {
-							return (<>
-								<li key={book.id}>
-									<Book
-										book={book}
-										changeShelfHandler={props.changeShelfHandler}
-										shelf={props.shelf.shelf}
-										selectValue={props.selectValue}
-									/>
-								</li>
-							</>)
-						}
-						)
-						}
+						{props.shelf?.books.map((book) => {
+							return (
+								<>
+									<li key={book.id}>
+										<Book
+											book={book}
+											changeShelfHandler={props.changeShelfHandler}
+											shelf={props.shelf.shelf}
+											selectValue={book.shelf}
+										/>
+									</li>
+								</>
+							);
+						})}
 					</ol>
 				</div>
 			</div>
 		</div>
-	)
-}
+	);
+};
 
-export default Shelf
+export default Shelf;
