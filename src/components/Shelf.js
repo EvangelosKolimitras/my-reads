5a5d0d0def686c7d7117a7a490c0d885b9f59ed6
Evangelos Kolimitras <evangeloskolimitras@outlook.com>
2020-12-15 12:02:25 +0100

    refactor: Badges added nex to to shelf titles

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Renderer from "./Renderer"
+import { Badge } from "react-bootstrap"
+
 const Shelf = props => {
-
 	const { label, books, currentCategoryShelf, onUpdateShelf, isLoaded, shelf } = props
-
 	const booksOfCurrentShelf = books.filter((book) => book.shelf === currentCategoryShelf)
-
 	return (
 		<div className="bookshelf">
-			<h2 className="bookshelf-title">{label} | {booksOfCurrentShelf.length === 1 ? `${booksOfCurrentShelf.length} book` : `${booksOfCurrentShelf.length} books`} </h2>
+			<h2 className="h2 bookshelf-title">{label} </h2>
+			<Badge
+				variant="secondary">{booksOfCurrentShelf.length === 1 ? `${booksOfCurrentShelf.length} book` : `${booksOfCurrentShelf.length} books`}</Badge>
 			<div className="bookshelf-books">
 				<ol className="books-grid">
 					{
