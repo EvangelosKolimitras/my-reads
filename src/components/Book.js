ce84b525cb6a9e714053d23fa93398f5a99b6686
Evangelos Kolimitras <evangeloskolimitras@outlook.com>
2020-12-15 11:53:28 +0100

    refactor: The Book component is now a Bootstrap Card

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Card, Form } from 'react-bootstrap'
 const Book = props => {
 	const { componentIsRenderedBy, authors, title, cover, shelfOfCurrentBook, id, onUpdateShelf, shelf } = props
 
@@ -29,25 +29,25 @@ const Book = props => {
 
 	return (
 		<li>
-			<div className="book">
-				<div className="book-top">
-					<div className="book-cover">
-						<img src={cover !== undefined ? cover.thumbnail : 'https://dummyimage.com/138x203/b3b3b3/ffffff.png&text=not+available'} alt={title} />
-					</div>
-					<div className="book-shelf-changer">
-						<select defaultValue={getShelfValue} onChange={(e) => onUpdateShelf(id, e.target.value)} >
+			<Card style={{ width: '15rem', boxShadow: '0 .4px 10px rgba(0,0,0)' }}>
+				<Card.Img height={250} variant="top" src={cover !== undefined ? cover.thumbnail : 'https://dummyimage.com/138x203/b3b3b3/ffffff.png&text=not+available'} alt={title} />
+				<Card.Body>
+					<Card.Title> <h2 className="h3">{title || "No title"}</h2> </Card.Title>
+					<Card.Text>{authors || "Anonymous"}</Card.Text>
+				</Card.Body>
+				<Form>
+					<Form.Group>
+						<Form.Control as="select" custom defaultValue={getShelfValue} onChange={(e) => onUpdateShelf(id, e.target.value)}>
 							<option disabled>Move to...</option>
 							<option disabled={bookShelf === "currentlyReading"} value="currentlyReading"> Currently Reading</option>
 							<option disabled={bookShelf === "wantToRead"} value="wantToRead">Want to Read</option>
 							<option disabled={bookShelf === "read"} value="read">Read</option>
 							<option value="none">None</option>
-						</select>
-					</div>
-				</div>
-				<div className="book-title">{title || "No title"} </div>
-				<div className="book-authors">{authors || "Anonymous"}</div>
-			</div>
-		</li>
+						</Form.Control>
+					</Form.Group>
+				</Form>
+			</Card>
+		</li >
 	)
 }
 
