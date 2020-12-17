ef956b1d09eda844b8a0701e1e75cc96b3f075d1
Evangelos Kolimitras <evangeloskolimitras@outlook.com>
2020-12-17 13:45:15 +0100

    refactor: Categories are now part of the state

--- src/components/Bookcase.js
+++ src/components/Bookcase.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Shelf from './Shelf';
 import { Container } from 'react-bootstrap'
+
 const Bookcase = props => {
 
-	const categories = [
+	const [categories] = useState([
 		{ id: 1, label: "Currently Reading", category: "currentlyReading" },
 		{ id: 2, label: "To read", category: "wantToRead" },
 		{ id: 3, label: "Read", category: "read" }
-	]
+	])
 
 	return (
 		<Container fluid="md">
